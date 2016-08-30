45ada448513a3be6c1465804faae17b00e6a9f30
Damon E. Drake <damon.e.drake@gmail.com>
2016-08-30 00:14:28 -0500

    Added Track.java details to Section 2 Homework.

--- Section 2/Homework/src/Track.java
+++ Section 2/Homework/src/Track.java
@@ -1,3 +1,41 @@
+/*
+ * This class takes us back to the good old days of Napster and Morpheus file sharing. The track below
+ * represents a MP3 audio track. Complete the todo statements and submit the file to moodle.
+ */
 public class Track {
+	// TODO create variables for the following fields: 
+	// track number
+	// title 
+	// artist
+	// duration ( in seconds ), 
+	// bit rate ( acceptable values are 128 kbps, 160 Kbps, 192 Kbps, 256 Kbps, 320 Kbps )
+	// TODO create getters/setters for the fields
+	
+	// TODO create a default constructor that prompts for all of the fields
+	// TODO when assigning a bit rate make sure it ends up as one of the acceptable values
+	// If the number is > 128 but < 160 it should be assigned 128, like wise for 160 - 191, 193 - 255,
+	// and 257 - 319
+	
+	// TODO create a function that returns a string rating of the bit rate
+	// 128, 160 = "Good"
+	// 192, 256 = "Better"
+	// 320 = "Best
+	
+	// TODO create a function that returns a string duration in minutes and seconds ( human readable )
 
+	// TODO create a function that prints out the track's information as follows:
+	/*
+	   {track number} - {title}
+	   {artist}
+	   {human readable duration }
+	   {bit rate} {bit rate rating}
+	   
+	   ex.
+	   
+	   2 - Nutshell
+	   Alice in Chains
+	   4 minutes and 19 seconds
+	   256 Kbps Better Quality	   
+	   
+	 */
 }
