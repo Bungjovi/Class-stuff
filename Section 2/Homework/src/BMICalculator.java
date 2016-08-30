6e40994f7e1ba4d8e6957f049a585bfcc81635c0
Damon E. Drake <damon.e.drake@gmail.com>
2016-08-29 23:16:26 -0500

    Moved Section 2 homework to appropriate folder.

---
+++ Section 2/Homework/src/BMICalculator.java
@@ -0,0 +1,24 @@
+/*
+	The body mass index (BMI) is commonly used by health and nutrition professionals to estimate 
+	human body fat in populations.  You will need to prompt the user for their weight and height. 
+	It is computed by taking the individual's weight (mass) in kilograms and dividing it by the 
+	square of their height in meters.
+	
+	Use the 4 categories below to to determine the output.
+	
+	BMI				Category
+	-----------------------------------
+	< 18.5			Under Weight
+	18.5 - 24.9		Normal
+	25.0 - 29.9		Overweight
+	30.0 or more	Obese
+	-----------------------------------
+ */
+
+public class BMICalculator {
+	//TODO Prompt for person's height in meters
+	//TODO Prompt for person's weight in kilograms
+	//TODO Calculate the person's BMI
+	//TODO Determine which category the person belongs to
+	//TODO Print out BMI and Category
+}
---
+++ Section 2/Homework/src/DogYears.java
@@ -0,0 +1,3 @@
+public class DogYears {
+
+}
